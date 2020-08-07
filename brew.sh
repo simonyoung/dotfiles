33adb06a26aba6eff58117070952addaf3172051
Simon Young <simon180@mac.com>
2020-08-07 18:24:01 +0100

    feat: Install Node.js with n and fix permissions

--- brew.sh
+++ brew.sh
@@ -5,6 +5,21 @@ brew upgrade
 
 BREW_PREFIX=$(brew --prefix)
 
+###################
+#     Node.js     #
+###################
+
+# Manage Node.js versions with n
+brew install n
+
+# Deal with Permission Denied errors that would stop normal operation
+sudo mkdir -p /usr/local/n
+sudo chown -R $(whoami) /usr/local/n
+sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+
+# Install the LTS version of Node.js
+n lts
+
 ##################
 #      Casks     #
 ##################
